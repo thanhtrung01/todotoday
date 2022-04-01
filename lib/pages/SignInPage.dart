383302e4ca8f4612c46ae1c746a4f88575d0ea04
Lê Quốc <quoc020721@gmail.com>
2022-04-01 13:29:36 +0700

    fix loi code1

---
+++ lib/pages/SignInPage.dart
@@ -0,0 +1,250 @@
+// ignore_for_file: file_names
+
+import 'package:todotoday/services/Auth_Service.dart';
+import 'package:todotoday/pages/SignUpPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
+import 'HomePage.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwdController = TextEditingController();
+  bool circular = false;
+  AuthClass authClass = AuthClass();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign In",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              buttonItem("assets/google.svg", "Continue with Google", 25, () {
+                authClass.googleSignIn(context);
+              }),
+              const SizedBox(
+                height: 15,
+              ),
+              buttonItem("assets/phone.svg", "Continue with Mobile", 30, () {}),
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                "Or",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              textItem("Email....", _emailController, false),
+              const SizedBox(
+                height: 15,
+              ),
+              textItem("Password...", _pwdController, true),
+              const SizedBox(
+                height: 40,
+              ),
+              colorButton(),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "If you don't have an Account? ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (builder) => SignUpPage()),
+                          (route) => false);
+                    },
+                    child: const Text(
+                      "SignUp",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Forgot Password?",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget colorButton() {
+    return InkWell(
+      onTap: () async {
+        try {
+          firebase_auth.UserCredential userCredential =
+              await firebaseAuth.signInWithEmailAndPassword(
+                  email: _emailController.text, password: _pwdController.text);
+          // ignore: avoid_print
+          print(userCredential.user.email);
+          setState(() {
+            circular = false;
+          });
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (builder) => HomePage()),
+              (route) => false);
+        } catch (e) {
+          final snackbar = SnackBar(content: Text(e.toString()));
+          ScaffoldMessenger.of(context).showSnackBar(snackbar);
+          setState(() {
+            circular = false;
+          });
+        }
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width - 100,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: const LinearGradient(colors: [
+            Color(0xfffd746c),
+            Color(0xffff9068),
+            Color(0xfffd746c)
+          ]),
+        ),
+        child: Center(
+          child: circular
+              ? const CircularProgressIndicator()
+              : const Text(
+                  "Sign In",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+
+  Widget buttonItem(
+      String imagepath, String buttonName, double size, Function onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width - 60,
+        height: 60,
+        child: Card(
+          color: Colors.black,
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: const BorderSide(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                imagepath,
+                height: size,
+                width: size,
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Text(
+                buttonName,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textItem(
+      String labeltext, TextEditingController controller, bool obscureText) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width - 70,
+      height: 55,
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        style: const TextStyle(
+          fontSize: 17,
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+          labelText: labeltext,
+          labelStyle: const TextStyle(
+            fontSize: 17,
+            color: Colors.white,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              width: 1.5,
+              color: Colors.amber,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
